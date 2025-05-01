eefda0550c6e0c8a4c3592d787527fe8a0d92863
satyajitc54 <satyajitc54@gmail.com>
2025-05-01 22:35:41 +0530

    routing fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import Header from './Header';
 import Contact from './Contact';
 import Projects from './Projects';
 import Home from './Home';
 import Footer from './Footer';
+import Scroll from "./Scroll"; 
+import About from './About';
 
 function App() {
   return (
     <Router>
       <Header />
+      <Scroll />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Contact" element={<Contact />} />
         <Route path="/Projects" element={<Projects/>}/>
+        <Route path="/About" element={<About/>}/>
       </Routes>
       <Footer/>
     </Router>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import "./Footer.css";
-import { Link } from "react-router-dom";
+import "./Home.css";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 return(
-    <div></div>
-);
-
+    <div className="container">
+      <div className="img-container">
+        <img
+          src={process.env.PUBLIC_URL + "/img/hero-area-enhanced.jpg"}
+          alt="about"
+          className="about-img"
+        />
+      </div>
+      <div className="text">"Your vision gets only <span className="one">ONE CHANCE</span>— let’s make it <span className="one">COUNT</span>."</div>
+      <div className="home-button">
+        <Link to="/About" className="know-more">
+          Know more
+        </Link>
+        <Link to="/Contact" className="know-more">
+          Contact now
+        </Link>
+      </div>
+    </div>
+ );
 };
 
 
